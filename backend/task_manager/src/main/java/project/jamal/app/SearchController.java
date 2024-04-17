c3e075204fb0dadaf970ca9bb6a2c8a4fce22e7f
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2024-04-17 15:08:03 -0400

    setup

---
+++ backend/task_manager/src/main/java/project/jamal/app/SearchController.java
@@ -0,0 +1,11 @@
+package project.jamal.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/search")
+@CrossOrigin
+public class SearchController {
+
+}
